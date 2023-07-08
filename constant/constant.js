87ddcb83c8967aafe63bdf3adc44c2a8598cdb77
Daria <dar.zavaliuk@gmail.com>
2023-07-08 19:54:19 +0300

    feat: add object with weather icons

---
+++ constant/constant.js
@@ -0,0 +1,20 @@
+export const apiKey = 'place your api key here from : https://www.weatherapi.com';
+
+export const weatherImages = {
+    'Partly cloudy': require('../assets/weather-icons/partlycloudy.png'),
+    'Moderate rain': require('../assets/weather-icons/moderaterain.png'),
+    'Patchy rain possible': require('../assets/weather-icons/moderaterain.png'),
+    'Sunny': require('../assets/weather-icons/sun.png'),
+    'Clear': require('../assets/weather-icons/sun.png'),
+    'Overcast': require('../assets/weather-icons/cloud.png'),
+    'Cloudy': require('../assets/weather-icons/cloud.png'),
+    'Light rain': require('../assets/weather-icons/moderaterain.png'),
+    'Moderate rain at times': require('../assets/weather-icons/moderaterain.png'),
+    'Heavy rain': require('../assets/weather-icons/heavyrain.png'),
+    'Heavy rain at times': require('../assets/weather-icons/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/weather-icons/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/weather-icons/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/weather-icons/heavyrain.png'),
+    'Mist': require('../assets/weather-icons/mist.png'),
+    'other': require('../assets/weather-icons/moderaterain.png')
+}
